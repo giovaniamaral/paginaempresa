d1b2e9ebb876d8ea3f554dbe1d556e92b13d224e
Giovani Amaral <94939971+giovaniamaral@users.noreply.github.com>
2023-10-16 21:38:02 -0300

    menu mobile finallized

---
+++ menu.js
@@ -0,0 +1,30 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+
+btnMenu.addEventListener('click', ()=>{
+  menu.classList.add('abrir-menu')
+})
+
+
+menu.addEventListener('click', ()=>{
+  menu.classList.remove('abrir-menu')
+})
+
+
+
+// Scroll descendo suavemente
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+          window.scrollTo({
+              top: targetElement.offsetTop,
+              behavior: 'smooth'
+          });
+      }
+  });
+});
